a28b97b307ea0991b0162a983dce5f57dd8f81be
PorvinderSingh <porvinder.1995@gmail.com>
2020-04-10 13:08:11 +0530

    Added my first Python File

---
+++ Python/App.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+print("Hello", name)
+Temp = 20
+if Temp > 30:
+    print("It's a hot day today!")
+elif Temp < 30:
+    print("It's a bit cold today!")
+else:
+    print("It's a fine day today!")
